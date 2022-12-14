0297cbd454ef92006aada8527585affd71260f1d
Gabriel <gamcode98@gmail.com>
2022-12-14 11:09:48 -0300

    styles fixed

--- src/utils/plans.ts
+++ src/utils/plans.ts
@@ -1,4 +1,7 @@
 import { IPlan } from '../interfaces/IPlan'
+import ArcadeIcon from '/src/assets/images/icon-arcade.svg'
+import AdvancedIcon from '/src/assets/images/icon-advanced.svg'
+import ProIcon from '/src/assets/images/icon-pro.svg'
 
 export const plans: IPlan[] = [
   {
@@ -6,20 +9,20 @@ export const plans: IPlan[] = [
     name: 'Arcade',
     priceByMonth: 9,
     priceByYear: 90,
-    image: '/src/assets/images/icon-arcade.svg',
+    image: ArcadeIcon,
   },
   {
     id: crypto.randomUUID(),
     name: 'Advanced',
     priceByMonth: 12,
     priceByYear: 120,
-    image: '/src/assets/images/icon-advanced.svg',
+    image: AdvancedIcon,
   },
   {
     id: crypto.randomUUID(),
     name: 'Pro',
     priceByMonth: 15,
     priceByYear: 150,
-    image: '/src/assets/images/icon-pro.svg',
+    image: ProIcon,
   },
 ]
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,8 +24,8 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'sidebar-mobile': "url('src/assets/images/bg-sidebar-mobile.svg')",
-        'sidebar-desktop': "url('src/assets/images/bg-sidebar-desktop.svg')",
+        'sidebar-mobile': "url('/assets/images/bg-sidebar-mobile.svg')",
+        'sidebar-desktop': "url('/assets/images/bg-sidebar-desktop.svg')",
       },
     },
   },
